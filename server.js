2ae5f8ce5016cf92d44578ece70bc514a0ec677c
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-30 14:10:51 -0700

    setup express server, handlebars, routing and controllers

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routing = require('./routing');
+const controllers = require('./controllers');
+
+// set environment variable port or set 3000 as default
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// MIDDLEWARE
+
+// make public static
+app.use(express.static('public'));
+
+// handle url encoded data; parse json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// routing and controllers
+app.use(routing);
+app.use(controllers);
+
+// start server and listen for client requests
+app.listen(PORT, function () {
+    // log (server-side) when server has started
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
